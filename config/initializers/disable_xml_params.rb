e68c7a695f70743c67f9174fa661eb2a2ec0ba92
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2019-12-02 13:48:28 -0500

    upgraded to rails 5

--- config/initializers/disable_xml_params.rb
+++ config/initializers/disable_xml_params.rb
@@ -1,1 +1,7 @@
-ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::Request.parameter_parsers = ActionDispatch::Request.parameter_parsers.except(:json)
+# ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
+# original_parsers = ActionDispatch::Request.parameter_parsers
+# xml_parser = -> (raw_post) { Hash.from_xml(raw_post) || {} }
+# new_parsers = original_parsers.merge(xml: xml_parser)
+# ActionDispatch::Request.parameter_parsers = new_parsers
