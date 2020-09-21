2479c2c28859983fffcc1313f2dc5035edb2b441
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-09-21 15:53:16 -0400

    made edit to stop error being thrown if last_modified attribute violates the no null contraint

--- app/models/category.rb
+++ app/models/category.rb
@@ -21,11 +21,12 @@ class Category < ActiveRecord::Base
       begin
         category = Category.find_by(nct_id: collected_nct_id, name: [@condition, @condition.underscore], grouping: [grouping, ''])
         category.update(grouping: @condition.underscore) if category && category.grouping.empty?
+        category.update(last_modified: Time.zone.now) if category
         category ||= Category.create(
                                       nct_id: collected_nct_id,
                                       name: @condition,
-                                      grouping: grouping)
-        category.update(last_modified: Time.zone.now)
+                                      grouping: grouping,
+                                      last_modified: Time.zone.now)
       rescue Exception => e
         puts "Failed: #{covid_nct_id}"
         puts "Error: #{e}"
