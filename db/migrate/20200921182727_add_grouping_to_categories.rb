d73ebb6aadc33037241bddf6e66d6ec770bf0418
Crystal Williams-Brown <crystal.williams.brown@gmail.com>
2020-09-21 14:55:50 -0400

    edited order of adding/removing indexes

--- db/migrate/20200921182727_add_grouping_to_categories.rb
+++ db/migrate/20200921182727_add_grouping_to_categories.rb
@@ -1,13 +1,13 @@
 class AddGroupingToCategories < ActiveRecord::Migration
   def up
-    add_column :categories, :grouping, :string, null: false
     remove_index :categories, name: "index_categories_on_nct_id_and_name"
+    add_column :categories, :grouping, :string, null: false
     add_index :categories, [:nct_id, :name, :grouping], unique: true
   end
 
   def down
-    remove_column :categories, :grouping
     remove_index :categories, name: "index_categories_on_nct_id_and_name_and_grouping"
+    remove_column :categories, :grouping
     add_index :categories, [:nct_id, :name], unique: true
   end
 end
